a887f42594c963b0d19bbda9e3732d2ce47de002
Drake <148833506+delliott18@users.noreply.github.com>
2025-04-11 15:29:04 -0700

    Added base class files to main directory

---
+++ CSE-360-Project/replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+    deps = [
+      pkgs.gradle
+      pkgs.gradle_5
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
